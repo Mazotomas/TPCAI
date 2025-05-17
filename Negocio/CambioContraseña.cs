2c052529d8572bbbbc03fb83736028824cf9be72
Agustintc99 <16TR94142561@campus.economicas.uba.ar>
2025-05-16 23:52:13 -0300

    Clase cambiar contraseña

---
+++ Negocio/CambioContraseña.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Persistencia;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class CambioContraseña
+    {
+        // private ContraseñaPersistencia persistencia = new ContraseñaPersistencia();
+
+
+        public string CambiarContraseña(string legajo, string nuevaContraseña, string contraseña)
+        {
+
+            ContraseñaPersistencia persistencia = new ContraseñaPersistencia();
+
+            if (nuevaContraseña.Length <= 8)
+            {
+                return "Debe contener 8 o más caracteres.";
+            }
+            if (contraseña == nuevaContraseña)
+            {
+                return "No puede ser igual a la última contraseña.";
+            }
+            bool actualizado = persistencia.ActualizarContraseña(legajo, nuevaContraseña);
+
+            if (actualizado == true)
+            {
+                return "Contraseña cambiada correctamente.";
+            }
+            return "Error al cambiar contraseña";
+
+
+        }
+
+    }
+
+   
+}
---
+++ Persistencia/ContraseñaPersistencia.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    public class ContraseñaPersistencia
+
+    {
+        public bool ActualizarContraseña(string legajo, string nuevoContraseña)
+        {
+            string archivoCsv = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DataBase", "Tablas", "credenciales.csv");
+            bool validacion = false;
+            try
+            {
+                var lineas = File.ReadAllLines(archivoCsv).ToList();
+
+                if (lineas.Count > 1)
+                {
+                    string encabezado = lineas[0];
+                    var nuevasLineas = new List<string> { encabezado };
+
+                    for (int i = 1; i < lineas.Count; i++)
+                    {
+                        string[] campos = lineas[i].Split(';');
+
+                        if (campos.Length == 5 && campos[0] == legajo)
+                        {
+
+                            campos[2] = nuevoContraseña;
+                            return validacion = true;
+                        }
+
+                        nuevasLineas.Add(string.Join(";", campos));
+                    }
+
+                    // Sobrescribe el archivo con los cambios
+                    File.WriteAllLines(archivoCsv, nuevasLineas);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error al actualizar la contraseña: " + ex.Message);
+            }
+            return validacion;
+
+        }
+    }
+}
